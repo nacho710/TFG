729287dfce7664f5a4b37d8170acaadb25271fb7
Nacho710 <ignbaena@ucm.es>
2020-12-01 21:05:35 +0100

    Form register captured and profile view created

---
+++ app/src/main/java/com/example/tfg/ProfileMenu.java
@@ -0,0 +1,15 @@
+package com.example.tfg;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileMenu extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_menu);
+
+    }
+}
--- app/src/main/java/com/example/tfg/Register.java
+++ app/src/main/java/com/example/tfg/Register.java
@@ -2,11 +2,14 @@ package com.example.tfg;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.sql.Date;
 
 
 public class Register extends AppCompatActivity {
@@ -18,8 +21,27 @@ public class Register extends AppCompatActivity {
 
     }
 
-    //public void goToRegister(View view){
-        //Intent i =  new Intent(this,Register.class);
-        //startActivity(i);
-    //}
+    public void createUser(View view){
+        /*final EditText userName = (EditText) findViewById(R.id.username);
+        String name = userName.getText().toString();
+        final EditText emailRegister = (EditText) findViewById(R.id.emailRegister);
+        String email = emailRegister.getText().toString();
+        final EditText passText = (EditText) findViewById(R.id.passRegister);
+        String pass = passText.getText().toString();
+        final EditText pass2Text = (EditText) findViewById(R.id.pass2Register);
+        String pass2 = pass2Text.getText().toString();
+        final EditText phoneText = (EditText) findViewById(R.id.phoneRegister);
+        String phone = phoneText.getText().toString();
+        final EditText dateText = (EditText) findViewById(R.id.dateRegister);
+        String date = dateText.getText().toString();
+        final EditText pesoText = (EditText) findViewById(R.id.pesoRegister);
+        String peso = pesoText.getText().toString();
+        final EditText altruaText = (EditText) findViewById(R.id.altruaRegister);
+        String altura = altruaText.getText().toString();
+        final Spinner dietasSpinner = (Spinner) findViewById(R.id.DietasSpinner);
+        String dietas = dietasSpinner.getSelectedItem().toString();*/
+
+        Intent i =  new Intent(this,ProfileMenu.class);
+        startActivity(i);
+    }
 }
